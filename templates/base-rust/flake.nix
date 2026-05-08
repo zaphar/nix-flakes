a47f9ee3b4cfa19572de3ef9fee56917221b0f75
Jeremy Wall <Jeremy@marzhillstudios.com>
2026-05-07 21:17:26 -0400

    feat: update rust template to use crane

--- templates/base-rust/flake.nix
+++ templates/base-rust/flake.nix
@@ -4,34 +4,30 @@
       url = "github:oxalica/rust-overlay";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-    naersk.url = "github:nix-community/naersk";
+    crane.url = "github:ipetkov/crane";
     flake-utils = {url = "github:numtide/flake-utils"; };
     flake-compat = { url = "github:edolstra/flake-compat"; flake = false; };
- 
+
   };
 
-  outputs = {nixpkgs, flake-utils, rust-overlay, naersk, ...}:
+  outputs = {nixpkgs, flake-utils, rust-overlay, crane, ...}:
   flake-utils.lib.eachDefaultSystem (system: let
     overlays = [
       rust-overlay.overlays.default
     ];
     pkgs = import nixpkgs { inherit system overlays; };
     rust-bin = pkgs.rust-bin.stable.latest.default;
-    naersk-lib = pkgs.callPackage naersk {
-        rustc = rust-bin;
-        cargo = rust-bin;
-    };
-    project = naersk-lib.buildPackage {
-    	src = ./.;
-      root = ./.;
+    craneLib = (crane.mkLib pkgs).overrideToolchain rust-bin;
+    project = craneLib.buildPackage {
+      src = craneLib.cleanCargoSource ./.;
+      strictDeps = true;
       nativeBuildInputs = [pkgs.pkg-config];
       buildInputs =
         (
           if pkgs.stdenv.isDarwin
           then with pkgs.darwin.apple_sdk.frameworks; [Security SystemConfiguration]
           else [pkgs.openssl]
-        )
-        ++ [rust-bin];
+        );
     };
   in
   {
